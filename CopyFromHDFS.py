52d850041a4961b1fb3aea5f1b6c9c527fe56d61
Edwin Yohanny Roa Avila <eroa@esri.co>
2016-09-21 04:28:56 -0500

    Actualización

--- CopyFromHDFS.py
+++ CopyFromHDFS.py
@@ -10,10 +10,7 @@ out_local_file_name =arcpy.GetParameterAsText(4)
 
 try :
     wh = WebHDFS(hostname, int(port),username)
-    arcpy.AddMessage("----- 1")
     fs = wh.getFileStatus(webhdfs_file_name)
-    arcpy.AddMessage("----- 2")
-    arcpy.AddMessage("fs['type']:{}".format(fs['type']))
     if fs['type'] == 'FILE':
         wh.copyFromHDFS(webhdfs_file_name, out_local_file_name, overwrite = bool(arcpy.gp.overwriteOutput))
     else: #DIRECTORY - copy all non-empty files from the directory (non-recursively) and append to the output file with NL
@@ -22,21 +19,13 @@ try :
         temp_file.close()
         
         with open(out_local_file_name, "wb") as out_local_file:
-            arcpy.AddMessage("----- 3")
             file_list = wh.listDirEx(webhdfs_file_name)
-            arcpy.AddMessage("----- 4")
             for file in file_list:
                 if file['length'] != 0 :
-                    arcpy.AddMessage("----- 4a")
-                    arcpy.AddMessage("-----File:{}".format( file['pathSuffix']))
                     wh.copyFromHDFS(webhdfs_file_name + '/' + file['pathSuffix'], temp_file_name, overwrite = True)
-                    arcpy.AddMessage("----- 55")
                     temp_file = open(temp_file_name, "rb")
                     shutil.copyfileobj(temp_file, out_local_file, length = 1024 * 1024)
-                    arcpy.AddMessage("----- 6")
-                    #out_local_file.write('\n')
                     temp_file.close()
-                    arcpy.AddMessage("----- 7")
         os.remove(temp_file_name)
 
     
